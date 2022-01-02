078546cd55eff437f89dbc2068e9a6312111beef
0siribix <nomail@me.pl>
2022-01-01 21:04:02 -0600

    Wow, I was using +25000 instead of -25000 SMH

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ if nether and nether.DEPTH_CEILING and nether.DEPTH_FLOOR then
 		label = "Nether Water Evaporate",
 		nodenames = {"group:water", "default:water_flowing", "default:river_water_flowing"},
 		min_y = -32000,
-		max_y = 25000,
+		max_y = -25000,
 		interval = 1,
 		chance = 1,
 		catch_up = false,
