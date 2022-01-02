e9b0d7d04f014ba2902425277ff6a3e47c11565b
0siribix <nomail@me.pl>
2022-01-01 20:33:35 -0600

    trying this again :-/

--- init.lua
+++ init.lua
@@ -10,11 +10,11 @@ if nether and nether.DEPTH_CEILING and nether.DEPTH_FLOOR then
 		chance = 1,
 		catch_up = false,
 		action = function(pos, node)
-			if pos.y < nether.DEPTH_CEILING and pos.y > nether.DEPTH_FLOOR then
+--			if pos.y < nether.DEPTH_CEILING and pos.y > nether.DEPTH_FLOOR then
 				minetest.set_node(pos, {name = "air"})
 				minetest.sound_play("default_cool_lava",
 					{pos = pos, max_hear_distance = 16, gain = 0.25}, true)
 			end
-		end
+--		end
 	})
 end
