f88f4b1568233ed6643c7d58c77e68ed1d990626
0siribix <nomail@me.pl>
2022-01-01 19:48:48 -0600

    hardcoded nether ceiling and floor

--- init.lua
+++ init.lua
@@ -1,13 +1,16 @@
-minetest.register_abm({
-	label = "Nether Water Evaporate",
-	nodenames = {"group:water", "default:water_flowing", "default:river_water_flowing"},
-	min_y = nether.DEPTH_FLOOR,
-	max_y = nether.DEPTH_CEILING,
-	interval = 1,
-	chance = 1,
-	catch_up = false,
-	action = function(pos, node)
-		minetest.set_node(pos, {name = "air"})
-		minetest.sound_play("default_cool_lava",
-			{pos = pos, max_hear_distance = 16, gain = 0.25}, true)
-	end})
+
+
+if nether then
+	minetest.register_abm({
+		label = "Nether Water Evaporate",
+		nodenames = {"group:water", "default:water_flowing", "default:river_water_flowing"},
+		max_y = 25000,
+		interval = 1,
+		chance = 1,
+		catch_up = false,
+		action = function(pos, node)
+			minetest.set_node(pos, {name = "air"})
+			minetest.sound_play("default_cool_lava",
+				{pos = pos, max_hear_distance = 16, gain = 0.25}, true)
+		end})
+end
